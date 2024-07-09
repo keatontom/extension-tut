7af7f3ba2ea867611f3c2b43d3e230aeacfe182c
keatontom <20kt34@queens.ca>
2024-07-08 21:33:23 -0400

    Changes to be committed: 	modified:   src/extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -82,6 +82,11 @@ export function activate(context: vscode.ExtensionContext) {
 			);
 		}));
 
+	context.subscriptions.push(
+		vscode.commands.registerCommand('extension-tut.showWebview', () => {
+			webviewPanel.createOrShow(context.extensionUri);
+		})
+	);
 	
 	context.subscriptions.push(
 		vscode.languages.registerCodeActionsProvider('queens', new Emojizer(), {
